a274563b6396e9b3a877377140016b4df39920be
t2023-m0108 <t2023-m0108@t2023-m0108ui-MacBookAir.local>
2024-03-07 16:01:20 +0900

    계산기 1단계

---
+++ app/src/main/java/com/example/myapplication/test.kt
@@ -0,0 +1,71 @@
+package com.example.myapplication
+
+fun main() {
+
+    while (true){
+        println("첫번째 숫자를 입력해주세요.")
+        var userNum1 = readLine()!!.toDouble()
+        println("연산자를 입력해주세요.")
+        var userOperator = readLine()!!
+        println("두번째 숫자를 입력해주세요.")
+        var userNum2 = readLine()!!.toDouble()
+
+        var result = Calculator(userNum1, userOperator, userNum2)
+
+        result.calculation()
+    }
+
+
+}
+
+open class Calculator(num1 : Double, operator : String, num2 : Double) {
+    var num1 : Double = num1
+    var operator : String = operator
+    var num2 : Double = num2
+    var content : Any = ""
+
+    init {
+        content  =
+            when(operator){
+                "+" -> num1+num2
+                "-" -> num1-num2
+                "*" -> num1*num2
+                "/" -> num1/num2
+                "%" -> num1%num2
+                else -> "잘못된 입력입니다."
+            }
+    }
+
+
+    open fun calculation() {
+        println(content)
+    }
+
+}
+
+//class AddOperation(num1 : Int, operator : String, num2 : Int) : Calculator(num1, operator, num2) {
+//
+//    init {
+//        content  =
+//            when(operator){
+//                "+" -> num1+num2
+//                "-" -> num1-num2
+//                "*" -> num1*num2
+//                "/" -> num1/num2
+//                "%" -> num1%num2
+//                else -> "잘못된 입력입니다."
+//            }
+//    }
+//}
+//
+//class SubstractOperation(num1 : Int, operator : String, num2 : Int) : Calculator(num1, operator, num2) {
+//
+//}
+//
+//class MultiplyOperation(num1 : Int, operator : String, num2 : Int) : Calculator(num1, operator, num2) {
+//
+//}
+//
+//class DivideOperation(num1 : Int, operator : String, num2 : Int) : Calculator(num1, operator, num2) {
+//
+//}
