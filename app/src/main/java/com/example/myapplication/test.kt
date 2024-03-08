65431b56ed5c595abd99dd37d722bf1aac5dbac4
t2023-m0108 <t2023-m0108@t2023-m0108ui-MacBookAir.local>
2024-03-08 12:40:11 +0900

    Lv3 계산기 최종

--- app/src/main/java/com/example/myapplication/test.kt
+++ app/src/main/java/com/example/myapplication/test.kt
@@ -4,15 +4,19 @@ fun main() {
 
     println("첫번째 숫자를 입력해주세요.")
     var userNum1 = readLine()!!.toDouble()
-    println("연산자를 입력해주세요.")
-    var userOperator = readLine()!!
+    var userOperator = ""
+    var userNum2 = 0.0
 
     while (true){
 
         println("두번째 숫자를 입력해주세요.")
-        var userNum2 = readLine()!!.toDouble()
+        userNum2 = readLine()!!.toDouble()
+
+        println("계산을 그만하려면 -1을, 아니라면 연산자를 입력해주세요.")
+        userOperator = readLine()!!
 
         var content = when(userOperator) {
+            "-1" -> break
             "+" -> AddOperation(userNum1, userOperator, userNum2)
             "-" -> SubtractOperation(userNum1, userOperator, userNum2)
             "*" -> MultiplyOperation(userNum1, userOperator, userNum2)
@@ -20,14 +24,16 @@ fun main() {
             "%" -> OddOperation(userNum1, userOperator, userNum2)
             else -> ElseOperation(userNum1, userOperator, userNum2)
         }
+
         content.calculate()
 
-        println("계산을 그만하려면 -1을, 아니라면 추가 연산자를 입력해주세요.")
-        userOperator = readLine()!!
-        if (userOperator == "-1"){
-            break
-        }else {
-            userNum1 = Calculator(userNum1, userOperator, userNum2).result.toDouble()
+        when(userOperator) {
+            "+" -> userNum1 = AddOperation(userNum1, userOperator, userNum2).result
+            "-" -> userNum1 = SubtractOperation(userNum1, userOperator, userNum2).result
+            "*" -> userNum1 = MultiplyOperation(userNum1, userOperator, userNum2).result
+            "/" -> userNum1 = DivideOperation(userNum1, userOperator, userNum2).result
+            "%" -> userNum1 = OddOperation(userNum1, userOperator, userNum2).result
+            else -> print("")
         }
     }
 
@@ -37,7 +43,7 @@ open class Calculator(num1 : Double, operator : String, num2 : Double) {
     var num1 : Double = num1
     var operator : String = operator
     var num2 : Double = num2
-    var result : Any = ""
+    var result : Double = 0.0
 
     init {
         this.result  = result
@@ -84,16 +90,15 @@ class MultiplyOperation(num1 : Double, operator : String, num2 : Double) : Calcu
 
 class DivideOperation(num1 : Double, operator : String, num2 : Double) : Calculator(num1, operator, num2) {
     init {
-        result = if (num2 != 0.0){
-            num1/num2
-        }else {
-            "0으로 나눌 수 없습니다."
-        }
+        result = num1 / num2
     }
-
     override fun calculate() {
 //        super.calculate()
-        println(result)
+        if(num2 != 0.0){
+            println(result)
+        }else {
+            println("0으로 나눌 수 없습니다.")
+        }
     }
 }
 
@@ -104,17 +109,21 @@ class OddOperation(num1 : Double, operator : String, num2 : Double) : Calculator
 
     override fun calculate() {
 //        super.calculate()
-        println(result)
+        if(num2 != 0.0){
+            println(result)
+        }else {
+            println("0으로 나눌 수 없습니다.")
+        }
     }
 }
 
 class ElseOperation(num1 : Double, operator : String, num2 : Double) : Calculator(num1, operator, num2) {
     init {
-        result = "잘못된 연산자입니다."
+        result = 0.0
     }
 
     override fun calculate() {
 //        super.calculate()
-        println(result)
+        println("잘못된 연산자입니다.")
     }
 }
